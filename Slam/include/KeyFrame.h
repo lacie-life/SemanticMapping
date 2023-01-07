d043e1e880a8fce0b74b456f5536e8e1ea482f50
lacie <00sao00ios00@gmail.com>
2023-01-07 17:26:05 +0700

    [Update] slam_lib built

--- Slam/include/KeyFrame.h
+++ Slam/include/KeyFrame.h
@@ -21,15 +21,15 @@
 #define KEYFRAME_H
 
 #include "MapPoint.h"
-#include "Thirdparty/DBoW2/DBoW2/BowVector.h"
-#include "Thirdparty/DBoW2/DBoW2/FeatureVector.h"
+#include "DBoW2/BowVector.h"
+#include "DBoW2/FeatureVector.h"
 #include "ORBVocabulary.h"
 #include "ORBextractor.h"
 #include "Frame.h"
 #include "KeyFrameDatabase.h"
 #include "ImuTypes.h"
 
-#include "GeometricCamera.h"
+#include "CameraModels/GeometricCamera.h"
 #include "SerializationUtils.h"
 
 #include <mutex>
