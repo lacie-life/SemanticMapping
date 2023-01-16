373b527bd55974ec29141b4d2c4a8be5bf79552d
lacie <00sao00ios00@gmail.com>
2023-01-16 14:18:10 +0700

    [Update] update pcl viewer

--- Slam/src/PointCloudMapping.cpp
+++ Slam/src/PointCloudMapping.cpp
@@ -8,6 +8,7 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <pcl/visualization/cloud_viewer.h>
 #include <pcl/common/projection_matrix.h>
+#include <pcl/io/pcd_io.h>
 #include "Converter.h"
 
 PointCloudMapping::PointCloudMapping(double resolution_)
@@ -43,18 +44,19 @@ void PointCloudMapping::insertKeyFrame(KeyFrame* kf, cv::Mat& color, cv::Mat& de
 pcl::PointCloud< PointCloudMapping::PointT >::Ptr PointCloudMapping::generatePointCloud(KeyFrame* kf, cv::Mat& color, cv::Mat& depth)
 {
     PointCloud::Ptr tmp(new PointCloud());
+
     // point cloud is null ptr
-    for ( int m=0; m<depth.rows; m+=3 )
+    for (int m = 0; m < depth.rows; m+=3)
     {
-        for ( int n=0; n<depth.cols; n+=3 )
+        for (int n = 0; n < depth.cols; n+=3)
         {
             float d = depth.ptr<float>(m)[n];
             if (d < 0.01 || d>10)
                 continue;
             PointT p;
             p.z = d;
-            p.x = ( n - kf->cx) * p.z / kf->fx;
-            p.y = ( m - kf->cy) * p.z / kf->fy;
+            p.x = (n - kf->cx) * p.z / kf->fx;
+            p.y = (m - kf->cy) * p.z / kf->fy;
 
             p.b = color.ptr<uchar>(m)[n*3];
             p.g = color.ptr<uchar>(m)[n*3+1];
@@ -64,13 +66,21 @@ pcl::PointCloud< PointCloudMapping::PointT >::Ptr PointCloudMapping::generatePoi
         }
     }
 
-    Eigen::Isometry3d T = ORB_SLAM3::Converter::toSE3Quat(kf->GetPose());
-    PointCloud::Ptr cloud(new PointCloud);
-    pcl::transformPointCloud( *tmp, *cloud, T.inverse().matrix());
-    cloud->is_dense = false;
+    std::cout << tmp->size() << std::endl;
 
-    cout<<"generate point cloud for kf "<<kf->mnId<<", size="<<cloud->points.size()<<endl;
-    return cloud;
+    Eigen::Isometry3d T = ORB_SLAM3::Converter::toSE3Quat(kf->GetPose());
+
+    PointCloud::Ptr cloud(new PointCloud);
+
+    pcl::transformPointCloud( *tmp, *tmp, T.inverse().matrix());
+
+    std::cout << T.inverse().matrix() << std::endl;
+
+    tmp->is_dense = false;
+
+    cout << "generate point cloud for kf " << kf->mnId << ", size=" << tmp->points.size() << endl;
+
+    return tmp;
 }
 
 void PointCloudMapping::viewer()
@@ -79,35 +89,39 @@ void PointCloudMapping::viewer()
     while(1)
     {
         {
-            unique_lock<mutex> lck_shutdown( shutDownMutex );
+            unique_lock<mutex> lck_shutdown(shutDownMutex);
             if (shutDownFlag)
             {
                 break;
             }
         }
         {
-            unique_lock<mutex> lck_keyframeUpdated( keyFrameUpdateMutex );
-            keyFrameUpdated.wait( lck_keyframeUpdated );
+            unique_lock<mutex> lck_keyframeUpdated(keyFrameUpdateMutex);
+            keyFrameUpdated.wait(lck_keyframeUpdated);
         }
 
         // keyframe is updated
         size_t N=0;
         {
-            unique_lock<mutex> lck( keyframeMutex );
+            unique_lock<mutex> lck(keyframeMutex);
             N = keyframes.size();
         }
 
-        for ( size_t i=lastKeyframeSize; i<N ; i++ )
+        for ( size_t i = lastKeyframeSize; i < N ; i++ )
         {
-            PointCloud::Ptr p = generatePointCloud( keyframes[i], colorImgs[i], depthImgs[i] );
+            PointCloud::Ptr p = generatePointCloud(keyframes[i], colorImgs[i], depthImgs[i]);
             *globalMap += *p;
         }
         PointCloud::Ptr tmp(new PointCloud());
-        voxel.setInputCloud( globalMap );
-        voxel.filter( *tmp );
-        globalMap->swap( *tmp );
-        viewer.showCloud( globalMap );
-        cout<<"show global map, size="<<globalMap->points.size()<<endl;
+
+        voxel.setInputCloud(globalMap);
+        voxel.filter(*tmp);
+        globalMap->swap(*tmp);
+
+        viewer.showCloud(globalMap);
+
+        cout << "show global map, size=" << globalMap->points.size() << endl;
+
         lastKeyframeSize = N;
     }
 }
